664a8f78b0d143257ad415c7b6347ca09fe9ec21
carlosmeloti <carlosmeloti@4c559dfd-04d3-4ea0-9dca-64372ba6ef48>
2018-09-13 22:38:17 +0000

    Primeiro Commit

---
+++ trunk/MOP-api-1/src/main/java/br/embrapa/MopApi1Application.java
@@ -0,0 +1,12 @@
+package br.embrapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MopApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MopApi1Application.class, args);
+	}
+}
